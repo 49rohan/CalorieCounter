d9fe02934ca89cfcd31f3524e7b7022d9e661241
Rohan Basnet <rohan.basnet@southeasttech.edu>
2024-10-18 19:56:23 -0500

    Created CalorieDBContext class in Models        * Added data in the database

---
+++ CalorieCounter/Models/Calorie.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalorieCounter.Models
+{
+    public class Calorie
+    {
+        [Required(ErrorMessage = "Please enter the food name.")]
+        public string Name {  get; set; }
+        public int? Calories {  get; set; }
+
+    }
+}
---
+++ CalorieCounter/Models/CalorieDBContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CalorieCounter.Models
+{
+    public class CalorieDBContext : DbContext
+    {
+        public CalorieDBContext
+            (DbContextOptions<CalorieDBContext> options) :base (options) 
+        {
+            
+        }
+
+        public DbSet<Calorie> Calories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Calorie>().HasData(
+                new Calorie
+                {
+                    Name = "Banana",
+                    Calories = 105
+                },
+                new Calorie
+                {
+                    Name = "Apple",
+                    Calories = 95
+                },
+                new Calorie
+                {
+                    Name = "Orange",
+                    Calories = 45
+                },
+                new Calorie
+                {
+                    Name = "Big Mac",
+                    Calories = 563
+                },
+                new Calorie
+                {
+                    Name = "Takis",
+                    Calories = 140
+                });
+        }
+    }
+}
