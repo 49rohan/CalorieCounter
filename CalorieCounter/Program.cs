3ce156800eeb45e5ab2f1da2a31cad8083f0502f
Rohan Basnet <rohan.basnet@southeasttech.edu>
2024-10-18 20:06:04 -0500

    Added ConnectionStrings

--- CalorieCounter/Program.cs
+++ CalorieCounter/Program.cs
@@ -1,7 +1,13 @@
+using CalorieCounter.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CalorieDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CalorieDBContext")));
 
 var app = builder.Build();
 
