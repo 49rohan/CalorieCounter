c935871ee429b09e08aeaee1e7c150f4e5d384a2
Rohan Basnet <rohan.basnet@southeasttech.edu>
2024-10-20 17:22:15 -0500

    Redesigned the site to make it look better

--- CalorieCounter/Models/Calorie.cs
+++ CalorieCounter/Models/Calorie.cs
@@ -5,6 +5,7 @@ namespace CalorieCounter.Models
     public class Calorie
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter the name of the food")]
         public string Name {  get; set; }
         public int? Calories {  get; set; }
 
